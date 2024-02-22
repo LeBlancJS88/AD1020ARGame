744f78b38e7b538eb9aeb4597c3dffb9580370ed
Jeremy L <112544082+LeBlancJS88@users.noreply.github.com>
2024-02-22 21:59:51 +0200

    UI Sprites and Script tweak

--- Assets/00_Project Files/Scripts/FaceFilter Scripts/FacePrefabChanger.cs
+++ Assets/00_Project Files/Scripts/FaceFilter Scripts/FacePrefabChanger.cs
@@ -7,36 +7,32 @@ public class FacePrefabChanger : MonoBehaviour
 {
     public SimpleScrollSnap simpleScrollSnap;
     public ARFaceManager arFaceManager;
-    public GameObject[] facePrefabs;
+    public GameObject[] facePrefabs; // Directly drag your face prefabs here in the inspector
 
-    // List to keep track of all instantiated face filter GameObjects
     private List<GameObject> instantiatedFaceFilters = new List<GameObject>();
 
     private void Start()
     {
-        simpleScrollSnap.OnPanelCentered.AddListener((previousPanelIndex, newPanelIndex) => {ChangeFacePrefab(newPanelIndex);});
+        simpleScrollSnap.OnPanelCentered.AddListener((previousPanelIndex, newPanelIndex) => { ChangeFacePrefab(newPanelIndex); });
     }
 
-    private void ChangeFacePrefab(int panelIndex)
+    private void ChangeFacePrefab(int newPanelIndex)
     {
-        // Deactivate all current face filter GameObjects
+        // Deactivate previous face filters
         foreach (GameObject faceFilter in instantiatedFaceFilters)
         {
-            faceFilter.SetActive(false); // Or Destroy(faceFilter) if you prefer
+            Destroy(faceFilter);
         }
-        instantiatedFaceFilters.Clear(); // Clear the list
+        instantiatedFaceFilters.Clear();
 
-        // Instantiate and set up the new face filter GameObjects
-        if (panelIndex >= 0 && panelIndex < facePrefabs.Length)
+        // Instantiate the new face filter
+        if (newPanelIndex >= 0 && newPanelIndex < facePrefabs.Length)
         {
-            GameObject newFaceFilterPrefab = facePrefabs[panelIndex];
+            GameObject newFaceFilterPrefab = facePrefabs[newPanelIndex];
             foreach (ARFace face in arFaceManager.trackables)
             {
                 GameObject instantiatedFilter = Instantiate(newFaceFilterPrefab, face.transform);
-                instantiatedFilter.transform.localPosition = Vector3.zero;
-                instantiatedFilter.transform.localRotation = Quaternion.identity;
-                instantiatedFilter.transform.localScale = Vector3.one;
-                instantiatedFaceFilters.Add(instantiatedFilter); // Add to the list
+                instantiatedFaceFilters.Add(instantiatedFilter);
             }
         }
     }
