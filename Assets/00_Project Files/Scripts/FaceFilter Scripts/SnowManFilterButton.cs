e5a1674e66a727f8dd072f325994c1371938a708
Jeremy L <112544082+LeBlancJS88@users.noreply.github.com>
2024-02-24 23:59:42 +0200

    Snowman face filter setup and audio

---
+++ Assets/00_Project Files/Scripts/FaceFilter Scripts/SnowManFilterButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SnowmanFilterButton : MonoBehaviour, IFaceFilterButton
+{
+    public GameObject faceFilterPrefab;
+    private FacePrefabChanger facePrefabChanger;
+
+    private void Start()
+    {
+        facePrefabChanger = FindObjectOfType<FacePrefabChanger>(); // Find and store a reference to the FacePrefabChanger script
+    }
+
+    public void ActivateFaceFilter()
+    {
+        facePrefabChanger.ChangeFacePrefab(faceFilterPrefab);
+    }
+
+    public void OnButtonPressed()
+    {
+        ActivateFaceFilter();
+    }
+}
