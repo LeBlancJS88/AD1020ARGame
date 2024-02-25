ba36ca856b41d363d63cd3b29a7c85e342f1a718
Jeremy L <112544082+LeBlancJS88@users.noreply.github.com>
2024-02-25 17:17:59 +0200

    Scuba diver filter setup

---
+++ Assets/00_Project Files/Scripts/FaceFilter Scripts/ScubaFilterButton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScubaFilterButton : MonoBehaviour, IFaceFilterButton
+{
+    public GameObject faceFilterPrefab;
+    private FacePrefabChanger facePrefabChanger;
+
+    private void Start()
+    {
+        // Find and store a reference to the FacePrefabChanger script
+        facePrefabChanger = FindObjectOfType<FacePrefabChanger>(); 
+    }
+
+    public void ActivateFaceFilter()
+    {
+        facePrefabChanger.ChangeFacePrefab(faceFilterPrefab);
+    }
+
+    public void OnButtonPressed()
+    {
+        ActivateFaceFilter();
+    }
+}
